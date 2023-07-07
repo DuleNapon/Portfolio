db048a46a6859121006f255da3970c82bfdd2416
DuleNapon <97521661+DuleNapon@users.noreply.github.com>
2023-07-07 09:49:18 +0000

    Modified certified section to use a for loop

--- main.py
+++ main.py
@@ -2,9 +2,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+certificates = [
+    {
+      "name": "Crash course on Python by Google",
+      "image": "Crash course on Python by Google.pdf"
+    },
+    {
+      "name": "Crash course on Python by Google",
+      "image": "Python.jpg"
+    },
+    {
+      "name": "Using Python to interact with the Operating System by Google",
+      "image": "Interacting with OS.jpg"
+    },
+    {
+      "name": "Introduction to Git and GitHub by Google",
+      "image": "Git and GitHub.jpg"
+    }
+]
+
 @app.route("/")
 def Home():
-    return render_template("home.html")
+    return render_template("home.html", certificates=certificates)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
