2ea84437e578773223fe34c340721cbb2a8e187e
DuleNapon <97521661+DuleNapon@users.noreply.github.com>
2023-07-14 22:14:40 +0000

    imported data from MySQL database to the website for the certificate section and galery and modified the files.

---
+++ bup.py
@@ -0,0 +1,88 @@
+certificates = [
+    {
+      "name": "Crash course on Python by Google",
+      "image": "Crash course on Python by Google.pdf"
+    },
+    {
+      "name": "Using Python to Interact with the Operating System by Google",
+      "image": "Using Python to Interact with the Operating System by Google.pdf"
+    },
+    {
+      "name": "Introduction to Git and GitHub by Google",
+      "image": "Introduction to Git and GitHub by Google.pdf"
+    },
+    {
+      "name": "SQL SoloLearn",
+      "image": "SQL SoloLearn.pdf"
+    },
+    {
+      "name": "Become a Project Manager",
+      "image": "Become a Project Manager.pdf"
+    },
+    {
+      "name": "Becoming a Six Sigma Green Belt",
+      "image": "Becoming a Six Sigma Green Belt.pdf"
+    },
+    {
+      "name": "Project Management for Beginners",
+      "image": "Project Management for Beginners.pdf"
+    },
+    {
+      "name": "The Basics of Scrum",
+      "image": "The Basics of Scrum.pdf"
+    },
+    {
+      "name": "8D and CAPA",
+      "image": "8D and CAPA.pdf"
+    },
+    {
+      "name": "Fishbone method",
+      "image": "Fishbone method.pdf"
+    },
+    {
+      "name": "5 Why",
+      "image": "5 Why.pdf"
+    },
+    {
+      "name": "Learning from mistakes I",
+      "image": "Learning from mistakes I.pdf"
+    },
+    {
+      "name": "Learning from mistakes II",
+      "image": "Learning from mistakes II.pdf"
+    },
+    {
+      "name": "Cost of Quality (COQ)",
+      "image": "Cost of Quality (COQ).pdf"
+    },
+    {
+      "name": "Lean Six Sigma",
+      "image": "Lean Six Sigma.pdf"
+    },
+    {
+      "name": "VSM – Value Stream Mapping",
+      "image": "VSM – Value Stream Mapping.pdf"
+    },
+    {
+      "name": "Analiza i optimizacija zaliha - ABC_XYZ",
+      "image": "Analiza i optimizacija zaliha - ABC_XYZ.pdf"
+    },
+    {
+      "name": "Kanban i interna logistika",
+      "image": "Kanban i interna logistika.pdf"
+    },
+    {
+      "name": "Mendix Rapid Developer Course",
+      "image": "Mendix Rapid Developer Course.pdf"
+    },
+    {
+      "name": "Hexagon metrology",
+      "image": "Hexagon metrology.pdf"
+    },
+    {
+      "name": "SOLIDWORKS 2019 Essential Training",
+      "image": "SOLIDWORKS 2019 Essential Training.pdf"
+    },
+    
+    
+]
---
+++ database.py
@@ -0,0 +1,49 @@
+import mysql.connector
+import os
+
+# MySQL connection configuration
+config = {
+    "database": "portfolio",
+    "username": "pl8285ki22iheu7fluzo",
+    "host": "aws.connect.psdb.cloud",
+    "password": "pscale_pw_eRWrprQYI8ERCDUgPn3ZLxSiAxedR66zPtWsShhsILv"
+}
+
+# Directory to save PDF files
+pdf_directory = 'static/Certificates'
+
+# Establish a connection to MySQL
+cnx = mysql.connector.connect(**config)
+cursor = cnx.cursor()
+
+# Retrieve data from the Certificates table
+select_query = "SELECT Cert_Name, Cert_File FROM Certificates"
+cursor.execute(select_query)
+data = cursor.fetchall()
+
+# Close the cursor and database connection
+cursor.close()
+cnx.close()
+
+# Prepare the certificates list of dictionaries
+certificates = []
+for row in data:
+    cert_name = row[0]
+    cert_file = row[1]
+
+    # Generate a unique file name based on the certificate name
+    file_name = f"{cert_name}.pdf"
+    file_path = os.path.join(pdf_directory, file_name)
+
+    # Save the PDF file to the server's file system
+    with open(file_path, 'wb') as file:
+        file.write(cert_file)
+
+    certificate = {
+        "name": cert_name,
+        "file_path": file_path
+    }
+
+    certificates.append(certificate)
+
+print(certificates)
